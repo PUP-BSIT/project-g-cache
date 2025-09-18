d08bcdb39cc8295cd2ace12ec48f47a5d42b37a9
Daniel Victorioso <danielvictorioso03@gmail.com>
2025-09-19 03:25:30 +0800

    add backend spring

---
+++ backend/pomodify/src/main/java/com/pomodify/PomodifyApplication.java
@@ -0,0 +1,13 @@
+package com.pomodify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomodifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomodifyApplication.class, args);
+	}
+
+}
---
+++ backend/pomodify/src/test/java/com/pomodify/PomodifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pomodify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomodifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
