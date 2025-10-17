8210acec5e5764bba44e25bf2dc43095044679fc
Daniel Victorioso <danielvictorioso03@gmail.com>
2025-10-18 02:43:27 +0800

    update backend - correct packaging

--- pomodify-backend/src/main/java/com/pomodify/backend/infrastructure/config/PasswordConfig.java
+++ pomodify-backend/src/main/java/com/pomodify/backend/infrastructure/config/PasswordConfig.java
@@ -1,4 +1,4 @@
-package com.pomodify.backend.config;
+package com.pomodify.backend.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
