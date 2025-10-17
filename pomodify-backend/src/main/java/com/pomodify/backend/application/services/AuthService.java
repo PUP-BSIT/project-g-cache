c5c8aefc92bdfdf974e1a3db7c282a21083fde76
Daniel Victorioso <danielvictorioso03@gmail.com>
2025-10-17 10:14:43 +0800

    rebuild backend structure

---
+++ pomodify-backend/src/main/java/com/pomodify/backend/application/services/AuthService.java
@@ -0,0 +1,13 @@
+package com.pomodify.backend.services;
+
+@Service
+@Slf4j
+public class AuthService {
+    @Autowired
+    private UserFactory userFactory;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public ApiResponse
+}
