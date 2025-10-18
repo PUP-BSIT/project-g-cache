8840ae4eb882b5cbe893ac0e3b6489a4e2b08395
Daniel Victorioso <danielvictorioso03@gmail.com>
2025-10-18 12:58:39 +0800

    remove unused imports

--- pomodify-backend/src/main/java/com/pomodify/backend/infrastructure/config/SecurityConfig.java
+++ pomodify-backend/src/main/java/com/pomodify/backend/infrastructure/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.pomodify.backend.infrastructure.config;
 
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,9 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.config.Customizer;
 
 /**
@@ -22,12 +17,6 @@ import org.springframework.security.config.Customizer;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    private final CustomJwtDecoder customJwtDecoder;
-
-    public SecurityConfig(CustomJwtDecoder customJwtDecoder) {
-        this.customJwtDecoder = customJwtDecoder;
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
