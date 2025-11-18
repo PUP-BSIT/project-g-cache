9f242b5e059c3bc7db1da919da8ffe38735d8e58
Hannah Lorainne Genandoy <lorainneh540@gmail.com>
2025-11-18 21:58:32 +0800

    add verify email modal component

---
+++ pomodify-frontend/src/app/shared/components/verify-email-modal/verify-email-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-email-modal',
+  imports: [],
+  templateUrl: './verify-email-modal.html',
+  styleUrl: './verify-email-modal.scss',
+})
+export class VerifyEmailModal {
+
+}
