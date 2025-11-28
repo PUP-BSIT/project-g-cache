d8c44278bfca004b88d5075b61bc7ae92ec0abd9
victorioso-daniel <danielvictorioso03@gmail.com>
2025-11-28 21:07:28 +0800

    add cors config (#46)

---
+++ pomodify-backend/src/main/java/com/pomodify/backend/infrastructure/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.pomodify.backend.infrastructure.config;
+
+import lombok.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:4200", "https://pomodify.site")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
